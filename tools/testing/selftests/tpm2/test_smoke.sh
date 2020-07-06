e6a14a3e3c467c8f1de1971bc36d11e3368f53c7
Jami Kettunen <jami.kettunen@protonmail.com>
2020-07-07 00:54:15 +0300

    Linux 5.8-rc4
    Tree: https://github.com/torvalds/linux/tree/dcb7fd82c75ee2d6e6f9d8cc71c52519ed52e258
    Tarball: https://github.com/torvalds/linux/archive/dcb7fd82c75ee2d6e6f9d8cc71c52519ed52e258.tar.gz

---
+++ tools/testing/selftests/tpm2/test_smoke.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# SPDX-License-Identifier: (GPL-2.0 OR BSD-3-Clause)
+
+# Kselftest framework requirement - SKIP code is 4.
+ksft_skip=4
+
+[ -e /dev/tpm0 ] || exit $ksft_skip
+
+python -m unittest -v tpm2_tests.SmokeTest
+python -m unittest -v tpm2_tests.AsyncTest
