3adc529d1ea650b8d5fedc35ac131e76a29e7bf0
AngeloGioacchino Del Regno <angelogioacchino.delregno@somainline.org>
2020-11-24 20:39:22 +0100

    soc: qcom: Add support for Core Power Reduction v3, v4 and Hardened
    This commit introduces a new driver, based on the one for cpr v1,
    to enable support for the newer Qualcomm Core Power Reduction
    hardware, known downstream as CPR3, CPR4 and CPRh, and support
    for MSM8998 and SDM630 CPU power reduction.
    
    In these new versions of the hardware, support for various new
    features was introduced, including voltage reduction for the GPU,
    security hardening and a new way of controlling CPU DVFS,
    consisting in internal communication between microcontrollers,
    specifically the CPR-Hardened and the Operating State Manager.
    
    The CPR v3, v4 and CPRh are present in a broad range of SoCs,
    from the mid-range to the high end ones including, but not limited
    to, MSM8953/8996/8998, SDM630/636/660/845.
    
    Signed-off-by: AngeloGioacchino Del Regno <angelogioacchino.delregno@somainline.org>

---
+++ include/soc/qcom/cpr.h
@@ -0,0 +1,17 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+/*
+ * Copyright (c) 2013-2020, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2019 Linaro Limited
+ * Copyright (c) 2021, AngeloGioacchino Del Regno
+ *                     <angelogioacchino.delregno@somainline.org>
+ */
+
+#ifndef __CPR_H__
+#define __CPR_H__
+
+struct cpr_ext_data {
+	int mem_acc_threshold_uV;
+	int apm_threshold_uV;
+};
+
+#endif /* __CPR_H__ */
