459ffee1a9a68851d4f4552542e3dda6ae7331b5
AngeloGioacchino Del Regno <angelogioacchino.delregno@somainline.org>
2020-12-15 01:06:27 +0100

    cpufreq: blacklist MSM8998 in cpufreq-dt-platdev
    Add the MSM8998 to the blacklist since the CPU scaling is handled
    out of this.
    
    Signed-off-by: AngeloGioacchino Del Regno <angelogioacchino.delregno@somainline.org>

--- drivers/cpufreq/cpufreq-dt-platdev.c
+++ drivers/cpufreq/cpufreq-dt-platdev.c
@@ -131,6 +131,7 @@ static const struct of_device_id blacklist[] __initconst = {
 
 	{ .compatible = "qcom,apq8096", },
 	{ .compatible = "qcom,msm8996", },
+	{ .compatible = "qcom,msm8998", },
 	{ .compatible = "qcom,qcs404", },
 	{ .compatible = "qcom,sc7180", },
 	{ .compatible = "qcom,sdm845", },
